38fc65530eb1a3bf5ab5d13c8d5f646f21d86a22
Jo Eungyeong <joeungyeong23@gmail.com>
2025-01-07 19:06:44 +0900

    Retrofit의 CallBack Hell

--- app/src/main/java/com/example/jetpack_ex/retrofit/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_ex/retrofit/MainActivity.kt
@@ -30,24 +30,48 @@ class MainActivity : AppCompatActivity() {
 
         // Retrofit을 통해 MyApi 인터페이스 생성
         val api = RetrofitInstance.getInstance().create(MyApi::class.java)
+
+        // 1. 유저 정보를 가져와서 (userId)
         api.getPost1().enqueue(object : Callback<Post> {
             override fun onResponse(call: Call<Post>, response: Response<Post>) {
                 Log.d("API1", response.body().toString())
+                // 2. userId 기반으로 데이터를 가져오고 가져온 닉네임값
+                api.getPostNumber(2).enqueue(object : Callback<Post> {
+                    override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                        Log.d("API2", response.body().toString())
+                        // 3. 닉네임 기반으로 데이터를 가져오고 유저의 댓글을 가져와서
+                        api.getPostNumber(3).enqueue(object : Callback<Post> {
+                            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                                Log.d("API3", response.body().toString())
+                                // 4. 유저의 댓글 기반으로, 대댓글
+                                api.getPostNumber(4).enqueue(object : Callback<Post> {
+                                    override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                                        Log.d("API4", response.body().toString())
+                                    }
+
+                                    override fun onFailure(call: Call<Post>, t: Throwable) {
+                                        Log.d("API4", "fail")
+                                    }
+
+                                })
+                            }
+
+                            override fun onFailure(call: Call<Post>, t: Throwable) {
+                                Log.d("API3", "fail")
+                            }
+
+                        })
+                    }
+
+                    override fun onFailure(call: Call<Post>, t: Throwable) {
+                        Log.d("API2", "fail")
+                    }
+
+                })
             }
 
             override fun onFailure(call: Call<Post>, t: Throwable) {
                 Log.d("API1", "fail")
-            }
-
-        })
-
-        api.getPostNumber(2).enqueue(object : Callback<Post> {
-            override fun onResponse(call: Call<Post>, response: Response<Post>) {
-                Log.d("API2", response.body().toString())
-            }
-
-            override fun onFailure(call: Call<Post>, t: Throwable) {
-                Log.d("API2", "fail")
             }
 
         })
